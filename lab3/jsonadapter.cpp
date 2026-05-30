2989799396e01f7c6803754770be64fb0e3db53b
BNS200 <nbufalov2005@gmail.com>
2026-05-30 19:48:20 +0700

    Adding method getSourceName, getDescription in jsonadapter and sqliteadapter

--- lab3/jsonadapter.cpp
+++ lab3/jsonadapter.cpp
@@ -1,3 +1,14 @@
 #include "jsonadapter.h"
 
-JSONDataAdapter::JSONDataAdapter(const QString& filePath, const QString& dateFormat) {}
+JSONDataAdapter::JSONDataAdapter(const QString& filePath, const QString& dateFormat) : m_filePath(filePath), m_dateFormat(dateFormat) {}
+
+QString JSONDataAdapter::getSourceName() const
+{
+    return "JSON File";
+}
+
+QString JSONDataAdapter::getDescription() const
+{
+    return QString("JSON: %1").arg(m_filePath);
+}
+
--- lab3/sqliteadapter.cpp
+++ lab3/sqliteadapter.cpp
@@ -1,3 +1,15 @@
 #include "sqliteadapter.h"
 
-SQLiteDataAdapter::SQLiteDataAdapter(const QString& dbPath, const QString& tableName, const QString& dateColumn, const QString& valueColumn, const QString& dateFormat) {}
+SQLiteDataAdapter::SQLiteDataAdapter(const QString& dbPath, const QString& tableName, const QString& dateColumn, const QString& valueColumn, const QString& dateFormat)
+    : m_dbPath(dbPath), m_tableName(tableName), m_dateColumn(dateColumn), m_valueColumn(valueColumn), m_dateFormat(dateFormat)
+{}
+
+QString SQLiteDataAdapter::getSourceName() const
+{
+    return "SQLite Database";
+}
+
+QString SQLiteDataAdapter::getDescription() const
+{
+    return QString("SQLite: %1\nTable: %2\nDate column: %3\nValue column: %4").arg(m_dbPath, m_tableName, m_dateColumn, m_valueColumn);
+}
