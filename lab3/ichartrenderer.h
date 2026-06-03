7cce0dfb71fe0de753e15a9d6fdf3ae99163fa31
BNS200 <nbufalov2005@gmail.com>
2026-06-03 10:50:12 +0700

    original IOC container implementation and ichartrenderer declaration

--- lab3/ichartrenderer.h
+++ lab3/ichartrenderer.h
@@ -1,4 +1,19 @@
 #ifndef ICHARTRENDERER_H
 #define ICHARTRENDERER_H
 
+#include <QtCharts/QChart>
+#include <QtCharts/QChartView>
+#include <memory>
+#include "idataprovider.h"
+
+
+class IChartRenderer {
+
+public:
+    virtual ~IChartRenderer() = default;
+    virtual QChart* createChart(const DataSet& data) = 0;
+    virtual QString getChartType() const = 0;
+    virtual void customizeChart(QChart* chart) = 0;
+};
+
 #endif // ICHARTRENDERER_H
