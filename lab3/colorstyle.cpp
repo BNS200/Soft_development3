dc96183351771b6bd75b08e74139c06306070511
BNS200 <nbufalov2005@gmail.com>
2026-06-04 08:38:20 +0700

    Adding implementation of colorstyle

--- lab3/colorstyle.cpp
+++ lab3/colorstyle.cpp
@@ -1,3 +1,42 @@
 #include "colorstyle.h"
+#include <QBrush>
+#include <QFont>
 
-colorstyle::colorstyle() {}
+void ColorStyle::configurePrinter(QPrinter& printer)
+{
+    printer.setColorMode(QPrinter::Color);
+    printer.setResolution(300);
+    printer.setPageSize(QPageSize::A4);
+    printer.setPageOrientation(QPageLayout::Landscape);
+}
+
+void ColorStyle::configurePainter(QPainter& painter)
+{
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setRenderHint(QPainter::SmoothPixmapTransform, true);
+    painter.setRenderHint(QPainter::TextAntialiasing, true);
+}
+
+void ColorStyle::customizeChart(QChart* chart)
+{
+    if (!chart) return;
+
+    chart->setTheme(QChart::ChartThemeLight);
+
+    chart->setBackgroundBrush(QBrush(Qt::white));
+    chart->setBackgroundVisible(true);
+
+    chart->legend()->setVisible(true);
+    chart->legend()->setBrush(QBrush(Qt::black));
+    chart->legend()->setAlignment(Qt::AlignBottom);
+
+    QFont titleFont = chart->titleFont();
+    titleFont.setBold(true);
+    titleFont.setPointSize(14);
+    chart->setTitleFont(titleFont);
+}
+
+QString ColorStyle::getStyleName() const
+{
+    return "Color";
+}
--- lab3/colorstyle.h
+++ lab3/colorstyle.h
@@ -1,10 +1,17 @@
 #ifndef COLORSTYLE_H
 #define COLORSTYLE_H
+#include "iprintstyle.h"
 
-class colorstyle
-{
+
+class ColorStyle : public IPrintStyle {
 public:
-    colorstyle();
+    ColorStyle() = default;
+
+    void configurePrinter(QPrinter& printer) override;
+    void configurePainter(QPainter& painter) override;
+    void customizeChart(QChart* chart) override;
+    QString getStyleName() const override;
 };
 
 #endif // COLORSTYLE_H
+x
